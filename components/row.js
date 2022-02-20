41460ad9a44c29a4972d5b3f5ae45a9696f09b1b
Drishtant Dubey <70314749+DRISHTANT-DUBEY@users.noreply.github.com>
2022-02-20 17:51:45 +0530

    Netflix error version 0.1 removed

--- components/row.js
+++ components/row.js
@@ -1,5 +1,6 @@
 import axios from '../lib/axios';
 import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
     const [movies, setMovies] = useState([]);
@@ -19,14 +20,17 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
             <h2 className='text-2xl text-white md:text-4xl font-medium md:mb-5'>{title}</h2>
             <div className='flex space-x-5 scrollbar-hide py-5 overflow-y-hidden overflow-x-scroll'>
                 {movies?.map((movie) => (
-                    <div className={`${isLargeRow ? 'flex-none h-auto py-6' : 'flex-none group w-80 h-48 hover:scale-110 duration-200 transition transform ease-out cursor-pointer hover:z-20'} `}>
-                        <img
+                    // eslint-disable-next-line react/jsx-key
+                    <div className={`${isLargeRow ? 'flex-none h-auto py-6' : 'flex-none group h-auto hover:scale-110 duration-200 transition transform ease-out cursor-pointer hover:z-20'} `}>
+                        <Image
                             className={`${isLargeRow ? 'h-48 md:w-48 md:h-72 object-contain cursor-pointer hover:scale-110 transition transform duration-150 ease-out rounded-lg' : 'absolute w-full h-full object-cover rounded-md hover:brightness-50 z-0'}`}
                             key={movie.id}
-                            src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path
+                            src={`https://image.tmdb.org/t/p/original/${movie.poster_path
                                 }`
                             }
                             alt={movie.name}
+                            width={192}
+                            height={288}
                         />
                         <h2 className={`${isLargeRow ? 'hidden' : 'z-50 absolute bottom-0 text-center w-full text-white text-lg font-medium hidden group-hover:block cursor-pointer px-3 bg-gradient-to-br from-transparent to-black '}`}>{movie.title || movie.name || movie.original_name}</h2>
                     </div>
